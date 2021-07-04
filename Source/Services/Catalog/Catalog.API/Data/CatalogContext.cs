b0afaafe9d04b29152b9ca790295cff5d6165ca7
mukhert <tamal.mukherjee@bsci.com>
2021-07-04 17:54:50 +0530

    Developed Catalog API

---
+++ Source/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettigs:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettigs:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettigs:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; set; }
+    }
+}
